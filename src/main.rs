de7c55025caa42cce7a948ac874c03eef2f5f43c
zombiefungus <divmermarlav@gmail.com>
2019-12-18 16:14:45 -0500

    solve day 14b

--- src/main.rs
+++ src/main.rs
@@ -20,19 +20,34 @@ fn parse_line(line: &str) -> (&str, (usize, Vec<(&str, usize)>)) {
 }
 
 fn main() {
-    assert_eq!(find_ore("data/test0.txt"), 165);
-    assert_eq!(find_ore("data/test1.txt"), 31);
-    assert_eq!(find_ore("data/test2.txt"), 13312);
-    assert_eq!(find_ore("data/test3.txt"), 180697);
-    assert_eq!(find_ore("data/test4.txt"), 2210736);
-    assert_eq!(find_ore("data/input.txt"), 1037742);
+    assert_eq!(find_ore(1, "data/test0.txt"), 165);
+    assert_eq!(find_ore(1, "data/test1.txt"), 31);
+    assert_eq!(find_ore(1, "data/test2.txt"), 13312);
+    assert_eq!(find_ore(1, "data/test3.txt"), 180697);
+    assert_eq!(find_ore(1, "data/test4.txt"), 2210736);
+    assert_eq!(find_ore(1, "data/input.txt"), 1037742);
+
+    assert_eq!(find_max_fuel(1_000_000_000_000, "data/test2.txt"), 82892753);
+    assert_eq!(find_max_fuel(1_000_000_000_000, "data/test3.txt"), 5586022);
+    assert_eq!(find_max_fuel(1_000_000_000_000, "data/test4.txt"), 460664);
+
+    println!(
+        "part 2: {}",
+        find_max_fuel(1_000_000_000_000, "data/input.txt")
+    );
+    // assert_eq!(find_ore(1, "data/test1.txt"), 31);
+    // assert_eq!(find_ore(1, "data/test2.txt"), 13312);
+    // assert_eq!(find_ore(1, "data/test3.txt"), 180697);
+    // assert_eq!(find_ore(1, "data/test4.txt"), 2210736);
+    // assert_eq!(find_ore(1, "data/input.txt"), 1037742);
 }
 
-fn find_ore(path: &str) -> usize {
-    let raw = read_to_string(path).expect("error reading file");
-    let reaction_info: HashMap<_, _> = raw.lines().map(parse_line).collect();
-    let mut shelf = HashMap::new();
-    let mut stack = vec![(1_usize, "FUEL")];
+fn make_fuel<'a>(
+    fuel_amount: usize,
+    shelf: &mut HashMap<&'a str, usize>,
+    reaction_info: &HashMap<&'a str, (usize, Vec<(&'a str, usize)>)>,
+) -> usize {
+    let mut stack = vec![(fuel_amount, "FUEL")];
     let mut ore = 0;
     while let Some((mut needed_amount, obj)) = stack.pop() {
         // how much do I have of the objective chemical
@@ -49,12 +64,15 @@ fn find_ore(path: &str) -> usize {
             needed_amount -= have;
             shelf.insert(obj, 0);
         }
+        // the factor to which multiple the producing reactiion in order to have
+        // enought of objective chemical
         let factor = needed_amount.div_euclid(*produced_amount)
             + if needed_amount.rem_euclid(*produced_amount) > 0 {
                 1
             } else {
                 0
             };
+        // update the shelf with whatever was left
         *shelf.entry(obj).or_insert(0) += factor * produced_amount - needed_amount;
 
         for (next_req_chem, next_req_am) in needed_chemicals {
@@ -66,8 +84,40 @@ fn find_ore(path: &str) -> usize {
             stack.push((next_need, next_req_chem));
         }
     }
+    ore
+}
+
+fn find_max_fuel(max_ore: usize, path: &str) -> usize {
+    let mut remaining_ore = max_ore;
+
+    let raw = read_to_string(path).expect("error reading file");
+    let reaction_info: HashMap<_, _> = raw.lines().map(parse_line).collect();
+    let mut shelf = HashMap::new();
+    let ore_one_fuel = make_fuel(1, &mut shelf, &reaction_info);
+    // restart the shelfs
+    shelf = HashMap::new();
+
+    // this is the amount of fuel that i can produce without using what is left
+    // in the shelf
+    let starting_fuel = max_ore.div_euclid(ore_one_fuel);
+    let mut max_fuel = starting_fuel;
+    // make the amount to update the shelfs with what remains
+    remaining_ore -= make_fuel(starting_fuel, &mut shelf, &reaction_info);
+    let mut aux_ore;
+    while {
+        aux_ore = make_fuel(1, &mut shelf, &reaction_info);
+        remaining_ore > aux_ore
+    } {
+        max_fuel += 1;
+        remaining_ore -= aux_ore;
+    }
+    max_fuel
+}
+fn find_ore(fuel_amount: usize, path: &str) -> usize {
+    let raw = read_to_string(path).expect("error reading file");
+    let reaction_info: HashMap<_, _> = raw.lines().map(parse_line).collect();
+    let mut shelf = HashMap::new();
+    let ore = make_fuel(fuel_amount, &mut shelf, &reaction_info);
     shelf.retain(|_, &mut v| v != 0);
-    println!("{} required ore", ore);
-    println!("the shelf: {:?}", shelf);
     ore
 }
