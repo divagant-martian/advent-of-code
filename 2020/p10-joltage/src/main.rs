31451945583d253cd1d824e7f3f389812182bb88
Diva <divma@protonmail.com>
2020-12-10 13:20:04 +0100

    day 10

--- 2020/p10-joltage/src/main.rs
+++ 2020/p10-joltage/src/main.rs
@@ -1,9 +1,8 @@
-use std::cmp::Ordering;
-use std::collections::VecDeque;
 use std::fs::read_to_string;
 
-fn load_sorted(filename: &'static str) -> Vec<usize> {
-    let mut nums: Vec<usize> = read_to_string(filename)
+type Num = u8;
+fn load_sorted(filename: &'static str) -> Vec<Num> {
+    let mut nums: Vec<Num> = read_to_string(filename)
         .unwrap()
         .lines()
         .map(str::parse)
@@ -14,7 +13,7 @@ fn load_sorted(filename: &'static str) -> Vec<usize> {
     nums
 }
 
-fn part1(nums: &[usize]) {
+fn part1(nums: &[Num]) {
     let mut ones = 0;
     let mut threes = 1;
     for w in nums.windows(2) {
@@ -32,98 +31,64 @@ fn part1(nums: &[usize]) {
     );
 }
 
-#[derive(Debug)]
-struct SearchState {
-    acc_diff: usize,
-    used_indexes: Vec<usize>,
-    last_index: usize,
-}
+use std::collections::HashMap;
+fn build_graph(nums: &[Num]) -> HashMap<Num, Vec<Num>> {
+    let mut graph: HashMap<Num, Vec<Num>> = HashMap::default();
 
-impl SearchState {
-    fn print(&self, prefix: &'static str, nums: &[usize]) {
-        let mut used_nums = Vec::with_capacity(self.used_indexes.len() + 1);
-        for ix in &self.used_indexes {
-            used_nums.push(nums[*ix]);
-        }
-        used_nums.push(nums[self.last_index]);
-        println!(
-            "{} acc_diff[{}], used_nums:[{:?}]",
-            prefix, self.acc_diff, used_nums
-        );
-    }
-}
-
-fn main() {
-    let data = "data/test1.txt";
-    let mut nums = load_sorted(data);
-    let target = *nums.last().unwrap() + 3;
-    dbg!(target);
-    nums.push(target);
-
-    let mut to_expand = VecDeque::new();
-    to_expand.push_back(SearchState {
-        acc_diff: 0,
-        used_indexes: vec![],
-        last_index: 0,
-    });
-
-    let mut working_sums = 0;
-
-    while let Some(SearchState {
-        acc_diff,
-        used_indexes,
-        last_index,
-    }) = to_expand.pop_front()
-    {
+    for last_index in 0..nums.len() {
         // we need to check what numbers can go after this one to expand them
+        let acc_diff = nums[last_index];
         let mut index_to_add = last_index + 1;
         loop {
             if let Some(n) = nums.get(index_to_add) {
                 let diff = n - nums[last_index];
                 if diff <= 3 {
-                    // this one fits the rules, now check the sum
-                    match (acc_diff + diff).cmp(&target) {
-                        Ordering::Less => {
-                            // this one could work, add this number to expand it
-                            let mut used_indexes = used_indexes.clone();
-                            used_indexes.push(last_index);
-                            let state = SearchState {
-                                acc_diff: acc_diff + diff,
-                                used_indexes,
-                                last_index: index_to_add,
-                            };
-                            state.print("adding ", &nums);
-                            to_expand.push_back(state);
-                            index_to_add += 1;
-                        }
-                        Ordering::Equal => {
-                            // this one works. Do not expand it
-                            working_sums += 1;
-                            let state = SearchState {
-                                acc_diff: acc_diff + diff,
-                                used_indexes,
-                                last_index: index_to_add,
-                            };
-                            state.print("WORKING ", &nums);
-                            break;
-                        }
-                        Ordering::Greater => {
-                            // does not work, do not expand more
-                            // println!("diff would be too large");
-                            break;
-                        }
-                    }
+                    graph.entry(acc_diff).or_insert(vec![]).push(*n);
+                    index_to_add += 1;
                 } else {
                     // anything beyond here does not fit the rules
-                    // println!("continuing won't fit the rules {}", n);
                     break;
                 }
             } else {
-                // println!("trying with index too large");
                 break;
             }
         }
     }
 
-    println!("working_ones {}", working_sums);
+    graph
+}
+
+fn count_paths(nums: &[Num], graph: &HashMap<Num, Vec<Num>>) -> usize {
+    let target = *nums.last().unwrap();
+    let mut known_counts: HashMap<Num, usize> = HashMap::new();
+    for n in nums.iter().rev() {
+        if *n == target {
+            known_counts.insert(*n, 1);
+            continue;
+        }
+
+        let count = graph
+            .get(n)
+            .unwrap()
+            .iter()
+            .map(|k| known_counts.get(k).unwrap())
+            .sum();
+
+        known_counts.insert(*n, count);
+    }
+
+    *known_counts.get(&0).unwrap()
+}
+
+fn part2(nums: &[Num]) {
+    let graph = build_graph(&nums);
+    let count = count_paths(&nums, &graph);
+    println!("working_ones {}", count);
+}
+
+fn main() {
+    let data = "data/chris.txt";
+    let nums = load_sorted(data);
+    part1(&nums);
+    part2(&nums);
 }
