3b0b893741e4ed01f445576980f9e3b94107ad24
Diva M <divma@protonmail.com>
2021-12-01 11:44:50 -0500

    d1 p2

--- 2021/p01-depth-measurement/src/main.rs
+++ 2021/p01-depth-measurement/src/main.rs
@@ -5,14 +5,17 @@ fn read_input(contents: String) -> Vec<usize> {
         .collect::<Vec<_>>()
 }
 
-fn num_increased(nums: &[usize]) -> usize {
-    nums.windows(2).filter(|slice| slice[1] > slice[0]).count()
+fn num_increased(nums: &[usize], window_size: usize) -> usize {
+    nums.windows(window_size)
+        .filter(|slice| slice[window_size - 1] > slice[0])
+        .count()
 }
 
 fn main() {
     let contents = std::fs::read_to_string("data/input1.txt").expect("File is ok");
     let nums = read_input(contents);
-    println!("ans: {}", num_increased(&nums));
+    println!("ans1: {}", num_increased(&nums, 2));
+    println!("ans2: {}", num_increased(&nums, 4));
 }
 
 #[cfg(test)]
@@ -22,6 +25,9 @@ mod tests {
     #[test]
     fn sample() {
         let nums = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        assert_eq!(7, num_increased(&nums));
+        assert_eq!(7, num_increased(&nums, 2));
+
+        let nums = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(5, num_increased(&nums, 4));
     }
 }
