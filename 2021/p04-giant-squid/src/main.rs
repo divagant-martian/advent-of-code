81226a32528ef547dcc0cf3ab78c93b281bff0c6
Diva M <divma@protonmail.com>
2021-12-07 22:50:25 -0500

    day 4 p1

---
+++ 2021/p04-giant-squid/src/main.rs
@@ -0,0 +1,162 @@
+fn main() {
+    let input = std::fs::read_to_string("./data/input.txt").expect("Input is ok");
+    let (draws, mut boards) = parse_input(&input);
+    let (winning_draw_position, winning_board_position) = play(&draws, &mut boards);
+    let score = calculate_score(
+        draws[winning_draw_position],
+        &boards[winning_board_position],
+    );
+    println!("Score: {}", score);
+}
+
+type Board = Vec<Vec<(u8, bool)>>;
+
+fn parse_board(input: &str) -> Board {
+    input
+        .trim()
+        .lines()
+        .map(|l| {
+            l.trim()
+                .split_ascii_whitespace()
+                .map(|num_str| num_str.parse::<u8>().expect("Numbers are ok"))
+                .map(|num| (num, false))
+                .collect()
+        })
+        .collect()
+}
+
+fn parse_input(input: &str) -> (Vec<u8>, Vec<Board>) {
+    let mut parts = input.split("\n\n");
+    let draws = parts
+        .next()
+        .expect("Draws must be present")
+        .trim()
+        .split(',')
+        .map(|num_str| num_str.parse::<u8>().expect("input number is ok"))
+        .collect();
+
+    let boards = parts.map(parse_board).collect();
+    (draws, boards)
+}
+
+fn mark_number(n: u8, board: &mut Board) -> bool {
+    let mut maybe_position = None;
+    for (row_n, row) in board.iter_mut().enumerate() {
+        for (col_n, (num, was_marked)) in row.iter_mut().enumerate() {
+            if *num == n {
+                *was_marked = true;
+                maybe_position = Some((row_n, col_n));
+                break;
+            }
+        }
+    }
+
+    if let Some((row, col)) = maybe_position {
+        let row_complete = board[row].iter().all(|(_n, was_marked)| *was_marked);
+        if row_complete {
+            return true;
+        }
+
+        let column_complete = board.iter().all(|row| row[col].1);
+        if column_complete {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn format_board(board: &Board) -> String {
+    board
+        .iter()
+        .map(|row| row.iter().map(|(n, _was_marked)| n).collect::<Vec<_>>())
+        .map(|row| format!("{:?}\n", row))
+        .collect()
+}
+
+fn play(
+    draws: &Vec<u8>,
+    boards: &mut Vec<Board>,
+) -> (
+    usize, /* winning draw position */
+    usize, /* winning board position */
+) {
+    for (i, n) in draws.iter().enumerate() {
+        for (board_number, board) in boards.iter_mut().enumerate() {
+            if mark_number(*n, board) {
+                return (i, board_number);
+            }
+        }
+    }
+    panic!("no one wins")
+}
+
+fn calculate_score(winning_n: u8, winning_board: &Board) -> u32 {
+    let non_marked_sum: u32 = winning_board
+        .iter()
+        .map(|row| {
+            row.iter()
+                .filter_map(|(n, was_marked)| (!was_marked).then(|| *n as u32))
+                .sum::<u32>()
+        })
+        .sum();
+    non_marked_sum * (winning_n as u32)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mark_bingo() {
+        let board = "
+            14 21 17 24  4
+            10 16 15  9 19
+            18  8 23 26 20
+            22 11 13  6  5
+             2  0 12  3  7
+        ";
+
+        let mut board = parse_board(board);
+        let draws = vec![7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21];
+        let winning_n = 24;
+
+        for n in draws {
+            assert!(!mark_number(n, &mut board));
+        }
+        assert!(mark_number(winning_n, &mut board));
+    }
+
+    #[test]
+    fn example() {
+        let input = "
+            7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+            22 13 17 11  0
+             8  2 23  4 24
+            21  9 14 16  7
+             6 10  3 18  5
+             1 12 20 15 19
+
+             3 15  0  2 22
+             9 18 13 17  5
+            19  8  7 25 23
+            20 11 10 24  4
+            14 21 16 12  6
+
+            14 21 17 24  4
+            10 16 15  9 19
+            18  8 23 26 20
+            22 11 13  6  5
+             2  0 12  3  7
+        ";
+
+        let (draws, mut boards) = parse_input(input);
+        let (winning_draw_position, winning_board_position) = play(&draws, &mut boards);
+        let score = calculate_score(
+            draws[winning_draw_position],
+            &boards[winning_board_position],
+        );
+        assert_eq!(score, 4512);
+    }
+}
