6dd88108f4f7cb792ebc1e874a8106edd4b01f99
Diva M <divma@protonmail.com>
2024-12-16 23:53:59 -0500

    add day 7 part a

---
+++ 2024/day_07/main.zig
@@ -0,0 +1,184 @@
+const std = @import("std");
+
+pub const std_options = .{
+    .log_level = .debug,
+};
+
+const Num: type = u64;
+
+const Op = enum(u1) {
+    add,
+    mul,
+
+    fn apply(self: Op, lhs: Num, rhs: Num) Num {
+        return switch (self) {
+            .add => lhs + rhs,
+            .mul => lhs * rhs,
+        };
+    }
+
+    pub fn format(self: Op, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = fmt;
+        _ = options;
+
+        const char: u8 = switch (self) {
+            .add => '+',
+            .mul => '*',
+        };
+
+        try writer.print("{u}", .{char});
+    }
+};
+
+const Equation = struct {
+    result: Num,
+    operands: std.ArrayList(Num),
+
+    const newErr = error{ missingResult, notEnoughOperands };
+
+    const WithOps = struct {
+        this: *const Equation,
+        ops: []const Op,
+
+        pub fn format(self: WithOps, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+            _ = fmt;
+            _ = options;
+
+            try writer.print("{d}", .{self.this.operands.items[0]});
+            for (self.ops, self.this.operands.items[1..]) |operator, operand| {
+                try writer.print(" {s} {d}", .{ operator, operand });
+            }
+        }
+    };
+
+    fn new(line: []const u8, allocator: std.mem.Allocator) !Equation {
+        var tokens = std.mem.tokenizeAny(u8, line, ": ");
+
+        const result_str = tokens.next() orelse return Equation.newErr.missingResult;
+        const result = try std.fmt.parseInt(Num, result_str, 10);
+
+        var operands = std.ArrayList(Num).init(allocator);
+        while (tokens.next()) |operand_str| {
+            const operand = try std.fmt.parseInt(Num, operand_str, 10);
+            try operands.append(operand);
+        }
+
+        if (operands.items.len < 2) {
+            return Equation.newErr.notEnoughOperands;
+        }
+
+        return Equation{ .result = result, .operands = operands };
+    }
+
+    fn is_solved_by(self: *const Equation, ops: []const Op) bool {
+        if (ops.len + 1 != self.operands.items.len) {
+            @panic("mismatched number of operands and operations");
+        }
+
+        return self.apply(ops) == self.result;
+    }
+
+    fn apply(self: *const Equation, ops: []const Op) Num {
+        var result = self.operands.items[0];
+        const len = ops.len;
+        for (ops, self.operands.items[1 .. len + 1]) |operator, operand| {
+            result = operator.apply(result, operand);
+        }
+
+        return result;
+    }
+
+    fn is_solvable(self: *const Equation) !bool {
+        var to_check = std.ArrayList(std.ArrayList(Op)).init(self.operands.allocator);
+
+        const empty_ops = std.ArrayList(Op).init(self.operands.allocator);
+        try to_check.append(empty_ops);
+
+        while (to_check.popOrNull()) |current_ops| {
+            if (current_ops.items.len + 1 != self.operands.items.len) {
+                const result = self.apply(current_ops.items);
+
+                // check if worth exploring
+                if (result > self.result) {
+                    continue;
+                }
+
+                var with_mul = try current_ops.clone();
+                try with_mul.append(.mul);
+
+                var with_add = try current_ops.clone();
+                try with_add.append(.add);
+
+                try to_check.append(with_mul);
+                try to_check.append(with_add);
+            } else if (self.is_solved_by(current_ops.items)) {
+                // solution found
+                const with_ops = Equation.WithOps{ .this = self, .ops = current_ops.items };
+                std.log.debug("Solution: {s} = {d}", .{ with_ops, self.result });
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    // process program name
+    _ = args.next().?;
+
+    var second_part = false;
+    if (args.next()) |part| {
+        if (std.mem.eql(u8, part, "a")) {
+            second_part = false;
+        } else if (std.mem.eql(u8, part, "b")) {
+            second_part = true;
+        } else {
+            std.log.err("parts a or b, you on drugs: {s}", .{part});
+            std.process.exit(1);
+        }
+    } else {
+        std.log.err("missing program part", .{});
+        std.process.exit(1);
+    }
+
+    var file_name: []const u8 = undefined;
+    if (args.next()) |name| {
+        file_name = name;
+    } else {
+        std.log.err("missing file name", .{});
+        std.process.exit(1);
+    }
+
+    var file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const data = try buf_reader.reader().readAllAlloc(allocator, std.math.maxInt(usize));
+
+    var equations = std.ArrayList(Equation).init(allocator);
+
+    var lines = std.mem.tokenizeScalar(u8, data, '\n');
+
+    while (lines.next()) |line| {
+        const eq = try Equation.new(line, allocator);
+        try equations.append(eq);
+    }
+
+    if (second_part) {
+        // std.log.info("part 2: {d}", .{loopy_obstacles.keys().len});
+    } else {
+        var total: usize = 0;
+        for (equations.items) |eq| {
+            if (try eq.is_solvable()) {
+                total += eq.result;
+            }
+        }
+        std.log.info("part 1: {d}", .{total});
+    }
+}
