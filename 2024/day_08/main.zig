90cd4167532c5427316b1926f4940a803fe5afb8
Diva M <divma@protonmail.com>
2024-12-17 21:58:48 -0500

    read input for day 8

---
+++ 2024/day_08/main.zig
@@ -0,0 +1,106 @@
+const std = @import("std");
+
+// pub const std_options = .{
+// .log_level = .debug,
+// };
+
+const Position = struct { i: usize, j: usize };
+
+const Antenna = union(enum) {
+    lowercase: u8,
+    uppercase: u8,
+    digit: u8,
+};
+
+const Map = struct {
+    antennas: std.AutoHashMap(Antenna, std.AutoArrayHashMap(Position, void)),
+    cols: usize,
+    lines: usize,
+
+    const newErr = error{ invalidChar, notRectangular };
+
+    fn new(data: []const u8, allocator: std.mem.Allocator) !Map {
+        var lines = std.mem.tokenizeScalar(u8, data, '\n');
+
+        var antennas = std.AutoHashMap(Antenna, std.AutoArrayHashMap(Position, void)).init(allocator);
+
+        var line_number: usize = 0;
+        var cols: usize = 0;
+        while (lines.next()) |line| : (line_number += 1) {
+            for (line, 0..) |char, col_number| {
+                const antenna = switch (char) {
+                    '.' => continue,
+                    'a'...'z' => Antenna{ .lowercase = char },
+                    'A'...'Z' => Antenna{ .uppercase = char },
+                    '0'...'9' => Antenna{ .digit = char },
+                    else => return Map.newErr.invalidChar,
+                };
+
+                const pos = Position{ .i = line_number, .j = col_number };
+                var entry = try antennas.getOrPut(antenna);
+                if (!entry.found_existing) {
+                    entry.value_ptr.* = std.AutoArrayHashMap(Position, void).init(allocator);
+                }
+                try entry.value_ptr.put(pos, {});
+            }
+            if (cols == 0) {
+                cols = line.len;
+            } else if (cols != line.len) {
+                return Map.newErr.notRectangular;
+            }
+        }
+        return Map{ .antennas = antennas, .lines = line_number, .cols = cols };
+    }
+};
+
+const Part = enum {
+    a,
+    b,
+
+    const Err = error{ missingProgramPart, unkownPart };
+
+    fn from_args(args: *std.process.ArgIterator) Err!Part {
+        if (args.next()) |part| {
+            if (std.mem.eql(u8, part, "a")) {
+                return Part.a;
+            } else if (std.mem.eql(u8, part, "b")) {
+                return Part.b;
+            } else {
+                return Part.Err.unkownPart;
+            }
+        } else {
+            return Part.Err.missingProgramPart;
+        }
+    }
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    // process program name
+    _ = args.next().?;
+
+    const part = try Part.from_args(&args);
+
+    var file_name: []const u8 = undefined;
+    if (args.next()) |name| {
+        file_name = name;
+    } else {
+        std.log.err("missing file name", .{});
+        std.process.exit(1);
+    }
+
+    var file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const data = try buf_reader.reader().readAllAlloc(allocator, std.math.maxInt(usize));
+
+    const map = try Map.new(data, allocator);
+
+    std.log.info("sol: {any}, len {d}", .{ part, map.antennas.count() });
+}
