ffd2f276bdbb73dd39a00257a49289985f5ed7af
Diva M <divma@protonmail.com>
2024-12-23 06:15:13 -0500

    calculate wide grid

--- 2024/day_15/main.zig
+++ 2024/day_15/main.zig
@@ -34,6 +34,29 @@ const Map = struct {
     grid: libgrid.Grid(Tile),
     robot_pos: libgrid.Position,
 
+    const ParseErr = error{ missingRobot, duplicatedRobot };
+
+    fn parse(data: []const u8, allocator: std.mem.Allocator) !Map {
+        const grid = try libgrid.Grid(Tile).new(data, Tile.parse, allocator);
+        var robot: ?libgrid.Position = null;
+        var iter = grid.iterator();
+        while (iter.next()) |entry| {
+            if (entry.value.* == .robot) {
+                if (robot == null) {
+                    robot = entry.position;
+                } else {
+                    return Map.ParseErr.duplicatedRobot;
+                }
+            }
+        }
+
+        if (robot) |robot_pos| {
+            return Map{ .grid = grid, .robot_pos = robot_pos };
+        } else {
+            return Map.ParseErr.missingRobot;
+        }
+    }
+
     /// Moves the robot returning whether the rebot moved.
     fn move_robot(self: *Map, direction: libgrid.Direction) bool {
         var maybe_candidate_pos = self.robot_pos.move(direction);
@@ -77,29 +100,6 @@ const Map = struct {
         }
     }
 
-    const ParseErr = error{ missingRobot, duplicatedRobot };
-
-    fn parse(data: []const u8, allocator: std.mem.Allocator) !Map {
-        const grid = try libgrid.Grid(Tile).new(data, Tile.parse, allocator);
-        var robot: ?libgrid.Position = null;
-        var iter = grid.iterator();
-        while (iter.next()) |entry| {
-            if (entry.value.* == .robot) {
-                if (robot == null) {
-                    robot = entry.position;
-                } else {
-                    return Map.ParseErr.duplicatedRobot;
-                }
-            }
-        }
-
-        if (robot) |robot_pos| {
-            return Map{ .grid = grid, .robot_pos = robot_pos };
-        } else {
-            return Map.ParseErr.missingRobot;
-        }
-    }
-
     fn sum_gps(self: *const Map) usize {
         var iter = self.grid.iterator();
         var sum: usize = 0;
@@ -112,7 +112,7 @@ const Map = struct {
         return sum;
     }
 
-    pub fn format(self: Map, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+    pub fn format(self: *const Map, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
         const formatter = self.grid.formatter(grid_fmt);
         try formatter.format(fmt, options, writer);
     }
@@ -131,6 +131,120 @@ const grid_fmt = struct {
             .robot => '@',
             .wall => '#',
             .box => 'O',
+        };
+        try std.fmt.formatAsciiChar(char, options, writer);
+    }
+}.foo;
+
+const WideTile = enum {
+    wall,
+    leftBox,
+    rightBox,
+    empty,
+    robot,
+};
+
+const WideMap = struct {
+    grid: libgrid.Grid(WideTile),
+    robot_pos: libgrid.Position,
+
+    fn from_map(map: Map) !WideMap {
+        defer map.grid.deinit();
+
+        var new_grid = try std.ArrayList(WideTile).initCapacity(map.grid.get_allocator(), map.grid.items().len * 2);
+        for (map.grid.items()) |tile| {
+            const wide_tiles: struct { WideTile, WideTile } = switch (tile) {
+                .wall => .{ .wall, .wall },
+                .empty => .{ .empty, .empty },
+                .robot => .{ .robot, .empty },
+                .box => .{ .leftBox, .rightBox },
+            };
+
+            const left_tile, const right_tile = wide_tiles;
+            new_grid.appendAssumeCapacity(left_tile);
+            new_grid.appendAssumeCapacity(right_tile);
+        }
+
+        const robot_pos = libgrid.Position{ .i = map.robot_pos.i, .j = map.robot_pos.j * 2 };
+        const wide_grid = libgrid.Grid(WideTile){ .grid = new_grid, .cols = map.grid.cols * 2 };
+        return WideMap{ .grid = wide_grid, .robot_pos = robot_pos };
+    }
+
+    /// Moves the robot returning whether the rebot moved.
+    fn move_robot(self: *WideMap, direction: libgrid.Direction) bool {
+        var maybe_candidate_pos = self.robot_pos.move(direction);
+
+        while (maybe_candidate_pos) |candidate_pos| {
+            const next_tile = self.grid.get_mut(candidate_pos) orelse return false;
+            switch (next_tile.*) {
+                .wall => return false,
+                .empty => {
+                    // robot simply Moves
+                    next_tile.* = .box;
+                    self.grid.get_mut(self.robot_pos).?.* = .empty;
+                    // to deal with the case of multiple moved boxes, we
+                    // recalculate where the robot ends, since candidate_pos
+                    // might not be next to robot_pos anymore.
+                    self.robot_pos = self.robot_pos.move(direction).?;
+                    self.grid.get_mut(self.robot_pos).?.* = .robot;
+                    return true;
+                },
+                .box => {
+                    // maybe we can move this, try the next position
+                    maybe_candidate_pos = candidate_pos.move(direction);
+                },
+                .robot => @panic("robot found robot while moving"),
+            }
+        }
+
+        return false;
+    }
+
+    fn move_robot_in_bulk(self: *WideMap, directions: []const libgrid.Direction, enable_debug: bool) void {
+        if (enable_debug) {
+            std.log.debug("wide grid:\n{d}", .{self});
+        }
+
+        for (directions) |dir| {
+            _ = self.move_robot(dir);
+            if (enable_debug) {
+                std.log.debug("moved to {any}\n{d}", .{ dir, self });
+            }
+        }
+    }
+
+    fn sum_gps(self: *const WideMap) usize {
+        var iter = self.grid.iterator();
+        var sum: usize = 0;
+        while (iter.next()) |entry| {
+            if (entry.value.* == .leftBox) {
+                sum += 100 * entry.position.i + entry.position.j;
+            }
+        }
+
+        return sum;
+    }
+
+    pub fn format(self: *const WideMap, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        const formatter = self.grid.formatter(wide_grid_fmt);
+        try formatter.format(fmt, options, writer);
+    }
+};
+
+const wide_grid_fmt = struct {
+    fn foo(
+        value: WideTile,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        const char: u8 = switch (value) {
+            .empty => '.',
+            .robot => '@',
+            .wall => '#',
+            .leftBox => '[',
+            .rightBox => ']',
         };
         try std.fmt.formatAsciiChar(char, options, writer);
     }
@@ -172,10 +286,21 @@ pub fn main() !void {
     var sections = std.mem.tokenizeSequence(u8, data, "\n\n");
 
     var map = try Map.parse(sections.next().?, allocator);
-    defer map.grid.deinit();
-
     const instructions = try parse_instructions(sections.next().?, allocator);
-    map.move_robot_in_bulk(instructions.items, true);
-    const sum_gps = map.sum_gps();
-    std.log.info("sum gps {d}", .{sum_gps});
+
+    switch (args.part) {
+        .a => {
+            defer map.grid.deinit();
+
+            map.move_robot_in_bulk(instructions.items, true);
+            const sum_gps = map.sum_gps();
+            std.log.info("sum gps {d}", .{sum_gps});
+        },
+        .b => {
+            const wide_map = try WideMap.from_map(map);
+            std.debug.print("wide map:\n{d}", .{wide_map});
+            const sum_gps = wide_map.sum_gps();
+            std.log.info("sum gps {d}", .{sum_gps});
+        },
+    }
 }
