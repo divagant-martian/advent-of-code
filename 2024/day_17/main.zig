d44a6ce006bb36320efb9b7a48620676b0678621
Diva M <divma@protonmail.com>
2024-12-29 14:16:32 -0500

    parse program to enum

--- 2024/day_17/main.zig
+++ 2024/day_17/main.zig
@@ -6,15 +6,28 @@ pub const std_options = .{
     .log_level = .debug,
 };
 
-pub const Program = struct {
+const Opcode = enum {
+    a_div,
+    b_div,
+    c_div,
+    b_xor,
+    b_str,
+    b_xor_c,
+    jnz,
+    out,
+};
+
+const Op = struct { opcode: Opcode, operand: u3 };
+
+const Program = struct {
     a_reg: usize,
     b_reg: usize,
     c_reg: usize,
 
     instruction_pointer: usize,
-    bytes: std.ArrayList(u3),
+    operations: std.ArrayList(Op),
 
-    fn parse(data: []const u8, allocator: std.mem.Allocator) error{ missingRegisterDec, missingRegister, invalidRegister, invalidByte, outOfMemory, missingProgramDec }!Program {
+    fn parse(data: []const u8, allocator: std.mem.Allocator) error{ missingRegisterDec, missingRegister, invalidRegister, invalidByte, outOfMemory, missingProgramDec, missingOperand }!Program {
         var sections = std.mem.tokenizeAny(u8, data, " ,\n");
 
         _ = sections.next() orelse return error.missingRegisterDec;
@@ -33,13 +46,27 @@ pub const Program = struct {
         const c_reg = std.fmt.parseInt(usize, c_reg_str, 10) catch return error.invalidRegister;
 
         _ = sections.next() orelse return error.missingProgramDec;
-        var bytes = std.ArrayList(u3).initCapacity(allocator, sections.rest().len / 2) catch return error.outOfMemory;
-        while (sections.next()) |byte_str| {
-            const byte = std.fmt.parseInt(u3, byte_str, 10) catch return error.invalidByte;
-            bytes.appendAssumeCapacity(byte);
+        var operations = std.ArrayList(Op).initCapacity(allocator, sections.rest().len / 4) catch return error.outOfMemory;
+        while (sections.next()) |opcode_str| {
+            const opcode_byte = std.fmt.parseInt(u3, opcode_str, 10) catch return error.invalidByte;
+            const opcode: Opcode = switch (opcode_byte) {
+                0 => .a_div,
+                1 => .b_xor,
+                2 => .b_str,
+                3 => .jnz,
+                4 => .b_xor_c,
+                5 => .out,
+                6 => .b_div,
+                7 => .c_div,
+            };
+
+            const operand_str = sections.next() orelse return error.missingOperand;
+            const operand = std.fmt.parseInt(u3, operand_str, 10) catch return error.invalidByte;
+
+            operations.appendAssumeCapacity(Op{ .opcode = opcode, .operand = operand });
         }
 
-        return Program{ .a_reg = a_reg, .b_reg = b_reg, .c_reg = c_reg, .instruction_pointer = 0, .bytes = bytes };
+        return Program{ .a_reg = a_reg, .b_reg = b_reg, .c_reg = c_reg, .instruction_pointer = 0, .operations = operations };
     }
 };
 
